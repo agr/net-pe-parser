c79f0c7556461f859e32471ad5751c3fd66c50bf
Andrei Grigorev <andrey.grigoriev@gmail.com>
2026-09-04 20:12:56 -0700

    Update to latest libraries and compiler, switched to SWC test transformer.

--- jest.config.ts
+++ jest.config.ts
@@ -1,15 +1,33 @@
-import type { JestConfigWithTsJest } from 'ts-jest'
+import type { Config } from 'jest';
 
-const config: JestConfigWithTsJest = {
+const config: Config = {
   extensionsToTreatAsEsm: ['.ts'],
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
     '^src/(.*)\\.js$': '<rootDir>/src/$1',
   },
   testEnvironment: "node",
-  preset: 'ts-jest/presets/js-with-ts-esm',
+  transform: {
+    '^.+\\.(t|j)sx?$': [
+      '@swc/jest',
+      {
+        jsc: {
+          target: 'es2022',
+          parser: {
+            syntax: 'typescript',
+            decorators: false,
+            dynamicImport: true,
+          },
+        },
+      },
+    ],
+  },
   testMatch: [
-    "**/test/**/*.[tj]s"
+    "<rootDir>/test/**/*.ts"
+  ],
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    "/dist/"
   ],
 };
 
